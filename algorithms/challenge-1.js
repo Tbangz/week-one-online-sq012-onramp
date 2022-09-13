390c5a517da259beabd17f30776a50f54beedb14
Tolu Balo <tolubalo36@gmail.com>
2022-09-13 10:21:38 +0000

    Update .gitignore and challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,7 @@
 function secondLargest(arr) {
-  
+  let secondLargestNumber = 0;
+  for (let i=0; i<arr.length; i++)
+    secondLargestNumber =arr[i]
+    return secondLargestNumber
 }
 module.exports = secondLargest
